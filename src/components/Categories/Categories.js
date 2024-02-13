22d881889974f8b21e8b0d3c7f409160158c33e1
utsaverma <utsav.verma@nagarro.com>
2024-02-13 19:08:49 +0530

    icons added to categories

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from 'react';
 import './Categories.css';
 import { getCategories } from '../../services/productServices';
 import { CategorySearchList } from '../CategorySearchList/CategorySearchList.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 
 const Categories = () => {
+
 
   const [data, setData] = useState([]);
   const [groupedData, setGroupedData] = useState({});
@@ -16,7 +19,7 @@ const Categories = () => {
     setData(getCategories());
   }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     const grouped = {};
     console.log(data)
 
@@ -27,68 +30,76 @@ const Categories = () => {
     });
 
     setGroupedData(grouped);
-  },[data])
+  }, [data])
 
 
   const handleGenderClick = (e, gender) => {
-    if(e.target.className === 'genderSelect'){
+    if (e.target.className === 'genderSelectWrapper') {
       setSelectedGender(selectedGender === gender ? null : gender);
       setSelectedCategory(null);
       setSelectedSubCategory(null);
-    } 
+    }
   };
 
   const handleCategoryClick = (e, category) => {
-    if(e.target.className === 'categorySelect'){
+    if (e.target.className === 'categorySelectWrapper') {
       setSelectedCategory(selectedCategory === category ? null : category);
       setSelectedSubCategory(null);
     }
   };
 
   const loadProducts = (e, subCat) => {
-    if(e.target.className === 'subCategorySelect'){
+    if (e.target.className === 'subCategorySelect') {
       setSelectedSubCategory(subCat);
     }
-    
   }
 
   return (
     <div className='minHeight categoryMainConatiner'>
-    <div className="grouped-dropdown">
-      <ul className="gender-list">
-        {Object.keys(groupedData).map((gender) => (
-          <li className="genderSelect" key={gender} onClick={(e) => handleGenderClick(e, gender)}>
-            {gender}
-            {selectedGender === gender && (
-              <ul className="category-list">
-                {Object.keys(groupedData[gender]).map((category) => (
-                  <li className="categorySelect" key={category} onClick={(e) => handleCategoryClick(e, category)}>
-                    {category}
-                    {selectedCategory === category && (
-                      <ul className="sub-category-list">
-                        {groupedData[gender][category].map((subCategory) => (
-                          <li className="subCategorySelect" key={subCategory} onClick={(e) => loadProducts(e, subCategory)}>{subCategory}</li>
-                        ))}
-                      </ul>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </li>
-        ))}
-      </ul>
-    </div>
-    {
-      selectedGender && selectedCategory &&  selectedSubCategory && 
-      <CategorySearchList 
-        gender={selectedGender}
-        category={selectedCategory}
-        subCategory={selectedSubCategory}
-      />
-    }
+      <div className="grouped-dropdown">
+        <ul className="gender-list">
+          {Object.keys(groupedData).map((gender) => (
+            <li className="genderSelect" key={gender} onClick={(e) => handleGenderClick(e, gender)}>
+              <WrappedSection val={gender} selectedVal={selectedGender} wrapperType="gender" />
+              {selectedGender === gender && (
+                <ul className="category-list">
+                  {Object.keys(groupedData[gender]).map((category) => (
+                    <li className="categorySelect" key={category} onClick={(e) => handleCategoryClick(e, category)}>
+                      <WrappedSection val={category} selectedVal={selectedCategory} wrapperType="category" />
+                      {selectedCategory === category && (
+                        <ul className="sub-category-list">
+                          {groupedData[gender][category].map((subCategory) => (
+                            <li className="subCategorySelect" key={subCategory} onClick={(e) => loadProducts(e, subCategory)}>
+                              {subCategory}
+                            </li>
+                          ))}
+                        </ul>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+      {
+        selectedGender && selectedCategory && selectedSubCategory &&
+        <CategorySearchList
+          gender={selectedGender}
+          category={selectedCategory}
+          subCategory={selectedSubCategory}
+        />
+      }
     </div>
   );
 };
+
+const WrappedSection = ({ val, selectedVal, wrapperType }) => {
+
+  return (
+    <span className={`${wrapperType}SelectWrapper`} title="click to expand"><FontAwesomeIcon icon={selectedVal === val ? faMinus : faPlus} /> &nbsp;{val}</span>
+  )
+}
 
 export default Categories;
